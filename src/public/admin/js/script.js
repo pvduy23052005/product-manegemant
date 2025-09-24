092f2dbe237d9690fe37db0d90f8509956826dc3
pvduy23052005 <162944866+pvduy23052005@users.noreply.github.com>
2025-09-24 16:03:39 +0700

    Css js sidebar

--- src/public/admin/js/script.js
+++ src/public/admin/js/script.js
@@ -1,1 +1,17 @@
-console.log("ok");
+const body = document.querySelector("body");
+if (body) {
+  const sidebar = document.querySelector(".sidebar");
+  const toggleSwitch = document.querySelector(".icon-toggle");
+  const downArrow = document.querySelector(".arrow");
+  const subMenu = body.querySelector(".sub-menu");
+
+  toggleSwitch.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+  });
+
+  downArrow.addEventListener("click", () => {
+    subMenu.classList.toggle("show-sub-menu");
+  });
+}
+
+console.log(body);
