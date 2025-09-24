d882f80aad2f08bab8cfffc32676a0a792cf0b64
pvduy23052005 <162944866+pvduy23052005@users.noreply.github.com>
2025-09-24 16:40:21 +0700

    Cấu hình database

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const routeAddmin = require("./src/routes/admin/index.route");
 require("dotenv").config();
+const Database = require("./src/config/database");
 
 const app = express();
 const port = process.env.PORT;
@@ -10,8 +11,9 @@ app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "src", "views"));
 app.use(express.static(path.join(__dirname, "src", "public")));
 
+Database.connectDatabase();
 routeAddmin(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
