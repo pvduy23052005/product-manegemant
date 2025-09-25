006cf4208cbb73a35d379f1a8228bf80607a2096
pvduy23052005 <162944866+pvduy23052005@users.noreply.github.com>
2025-09-25 15:44:27 +0700

    update css close sidebar

--- src/public/admin/js/script.js
+++ src/public/admin/js/script.js
@@ -5,28 +5,21 @@ if (body) {
   const downArrow = document.querySelector(".arrow");
   const subMenu = body.querySelector(".sub-menu");
 
+  if (localStorage.getItem("closeSidebar") == "close") {
+    sidebar.classList.add("close");
+  }
+
   toggleSwitch.addEventListener("click", () => {
     sidebar.classList.toggle("close");
+
+    if (sidebar.classList.contains("close")) {
+      localStorage.setItem("closeSidebar", "close");
+    } else {
+      localStorage.setItem("closeSidebar", "open");
+    }
   });
 
   downArrow.addEventListener("click", () => {
     subMenu.classList.toggle("show-sub-menu");
   });
 }
-
-let lastScrollTop = 0;
-const header = document.querySelector(".header"); // chỉnh selector theo đúng file pug của bạn
-
-window.addEventListener("scroll", function () {
-  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-  if (scrollTop > lastScrollTop) {
-    // cuộn xuống -> ẩn header
-    header.style.top = "-60px"; // đẩy header ra ngoài màn hình
-  } else {
-    // cuộn lên -> hiện lại header
-    header.style.top = "0";
-  }
-
-  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // tránh số âm
-});
