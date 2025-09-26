ee0ce03e04da98e649414c6ef2701b1cf6056074
pvduy23052005 <162944866+pvduy23052005@users.noreply.github.com>
2025-09-26 10:04:47 +0700

    Giao diện trang product

--- src/controllers/admin/product.controller.js
+++ src/controllers/admin/product.controller.js
@@ -2,16 +2,16 @@ const Product = require("../../models/product.model");
 
 //[get] /admin/product
 module.exports.index = async (req, res) => {
+  
   let find = {
     deleted: false,
-    status: "active",
   };
 
   const listProduct = await Product.find(find);
 
-  // console.log(listProduct);
 
   res.render("admin/pages/product/index", {
     title: "Products",
+    listProduct : listProduct
   });
 };
