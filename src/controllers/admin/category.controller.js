a58dbf994b5c32b662ff9d643a4e78931b69fad1
pvduy23052005 <162944866+pvduy23052005@users.noreply.github.com>
2025-11-08 16:20:17 +0700

    Xóa nhiều và thay đổi nhiều danh mục

--- src/controllers/admin/category.controller.js
+++ src/controllers/admin/category.controller.js
@@ -1,6 +1,6 @@
-const { model } = require("mongoose");
 const Category = require("../../models/category.model");
 
+//[get] /admin/categor
 module.exports.index = async (req, res) => {
   let find = {
     deleted: false,
@@ -29,6 +29,56 @@ module.exports.changeStatus = async (req, res) => {
       }
     );
     req.flash("success", "Thay đổi trạng thái thành công");
+  } catch (error) {
+    req.flash("error", "Vui lòng thử lại");
+  }
+  res.redirect("/admin/category");
+};
+
+//[patch] /admin/category
+module.exports.changeMulti = async (req, res) => {
+  const listIdItem = JSON.parse(req.body.listIdItem);
+
+  console.log(listIdItem);
+  try {
+    switch (listIdItem.statusChange) {
+      case "active":
+        await Category.updateMany(
+          {
+            _id: { $in: listIdItem.listId },
+          },
+          {
+            status: "active",
+          }
+        );
+        req.flash("success", "Cập nhật nhiều bản ghi thành công");
+        break;
+      case "inactive":
+        await Category.updateMany(
+          {
+            _id: { $in: listIdItem.listId },
+          },
+          {
+            status: "inactive",
+          }
+        );
+        req.flash("success", "Cập nhật thành công");
+        break;
+      case "delete":
+        await Category.updateMany(
+          {
+            _id: { $in: listIdItem.listId },
+          },
+          {
+            deleted: true,
+          }
+        );
+        req.flash("success", "Xóa nhiều thành công");
+        break;
+      default:
+        req.flash("error", "Vui lòng chọn hành động");
+        break;
+    }
   } catch (error) {
     req.flash("error", "Vui lòng thử lại");
   }
--- src/routes/admin/category.route.js
+++ src/routes/admin/category.route.js
@@ -6,4 +6,5 @@ router.get("/", controller.index);
 
 router.patch("/change-status/:status/:id", controller.changeStatus);
 
+router.patch("/change-multi", controller.changeMulti);
 module.exports = router;
