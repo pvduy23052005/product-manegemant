9e36b5cd0c6f40052ef022b5567cada0907f8569
pvduy23052005 <162944866+pvduy23052005@users.noreply.github.com>
2025-11-23 09:41:58 +0700

    Clean code

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 
-require("dotenv").config(); // load biến từ .env
-
 module.exports.connectDatabase = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
