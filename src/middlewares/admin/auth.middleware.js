0ebfbb7f993b555f34be5475cd98d705d12f0b06
pvduy23052005 <162944866+pvduy23052005@users.noreply.github.com>
2025-11-25 08:19:38 +0700

    Update phân quyền

--- src/middlewares/admin/auth.middleware.js
+++ src/middlewares/admin/auth.middleware.js
@@ -1,4 +1,5 @@
 const Account = require("../../models/account.model");
+const Role = require("../../models/role.model");
 
 module.exports.authMiddleware = async (req, res, next) => {
   const token = req.cookies.token;
@@ -8,16 +9,22 @@ module.exports.authMiddleware = async (req, res, next) => {
     return;
   }
 
-  const email = await Account.findOne({
+  const user = await Account.findOne({
     token: token,
     deleted: false,
     status: "active",
-  });
+  }).select("-password");
 
-  if (!email) {
+  if (!user) {
     res.redirect("/admin/auth/login");
     return;
   }
+
+  const role = await Role.findOne({
+    _id: user.role_id,
+  });
+  res.locals.user = user;
+  res.locals.role = role;
 
   next();
 };
