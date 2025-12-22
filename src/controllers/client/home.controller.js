2cf07078aa656770b7eda0dbee989b7e2e75aad8
pvduy23052005 <162944866+pvduy23052005@users.noreply.github.com>
2025-12-22 10:27:59 +0700

    Add : tìm kiếm sản phẩm  client

--- src/controllers/client/home.controller.js
+++ src/controllers/client/home.controller.js
@@ -1,12 +1,19 @@
 const Product = require("../../models/product.model");
 const Category = require("../../models/category.model");
+const helper = require("../../helpers/helperSerach");
 
 // [get] /
 module.exports.index = async (req, res) => {
+  const keySearch = req.query.keySearch;
+
   let find = {
     deleted: false,
     status: "active",
   };
+
+  if (keySearch) {
+    find.title = helper.helperSearch(req).title;
+  }
 
   const products = await Product.find(find).select(
     "-createBy  -updatedBy  -deletedBy"
@@ -17,6 +24,7 @@ module.exports.index = async (req, res) => {
     title: "Home",
     products: products,
     categories: categories,
+    keySearch: keySearch || "",
   });
 };
 
--- src/public/client/js/script.js
+++ src/public/client/js/script.js
@@ -14,3 +14,22 @@ if (successMessage) {
     successMessage.classList.remove("show");
   }, 5000);
 }
+
+// search
+const formSearch = document.querySelector("[form-search]");
+if (formSearch) {
+  formSearch.addEventListener("submit", (e) => {
+    const url = new URL(window.location.href);
+    e.preventDefault();
+    const keyWord = e.target[0].value;
+
+    if (keyWord) {
+      url.searchParams.set("keySearch", keyWord);
+    } else {
+      url.searchParams.delete("keySearch");
+    }
+    e.target[0].value = keyWord.trim();
+    window.location.href = url.href;
+  });
+}
+// end search
